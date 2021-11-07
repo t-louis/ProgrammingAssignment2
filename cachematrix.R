1864ea8bc88e853c8c8e99b09018be0f48e6d364
t-louis <louis.thiam@gmail.com>
2021-11-07 09:37:23 +0100

    Inital trial commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,51 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    ## Initialize cached value in scope
+    ## of the function
+    xDim = dim(x)
+    cached <- matrix(, nrow=xDim[1], ncol=xDim[2])
+    
+    ## Define function handlers for retrieve, compute,
+    ## and caching any operation on a matrix. We are
+    ## not assuming any particular operation. We are
+    ## only caching the result of the operation based
+    ## on passed matrix. The computation is done outside
+    ## of this function
+    setMatrix <- function(y) {
+        x <<- y
+        yDim = dim(y)
+        cached <<- matrix(, nrow=yDim[1], ncol=yDim[2])
+    }
+    getMatrix <- function() x
+    setMatrixFunc <- function(computed) cached <<- computed
+    getMaxtrixFunc <- function() cached
+    
+    ## Return the list of function handlers
+    list(setMatrix = setMatrix, getMatrix = getMatrix,
+         setMatrixFunc = setMatrixFunc,
+         getMatrixFunc = getMatrixFunc)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ <- function(x, ...) {
+    
+    ## Retrieve the cached inverse if already
+    ## processed
+    matInv <- x$getMatrixFunc()
+    if(!is.null(matInv)) {
+        message("getting cached data")
+        return(matInv)
+    }
+    
+    ## Compute the matrix inverse
+    currentMatrix <- x$getMatrix()
+    matInv <- solve(currentMatrix)
+    x$setMatrixFunc(matInv)
+    
+    ## Return a matrix that is the inverse of 'x'
+    matInv
 }
